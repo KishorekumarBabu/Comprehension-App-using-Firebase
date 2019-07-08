d337499e173c734f5cd1078fb1c702adf0c89714
KishorekumarBabu <50402067+KishorekumarBabu@users.noreply.github.com>
2019-07-08 10:57:45 +0530

    Updated files

--- src/Components/contestants/contestantsList.js
+++ src/Components/contestants/contestantsList.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ContestantSummary from './contestantSummary';
-import { Link } from 'react-router-dom';;
+import React from "react";
+import ContestantSummary from "./contestantSummary";
+import { Link } from "react-router-dom";
 
-const ContestantList = ({contestants}) => {
+const ContestantList = ({ contestants }) => {
   return (
-      <div className="project-list section">
-        {contestants && contestants.map(contestant => {
+    <div className="project-list section">
+      {contestants &&
+        contestants.map(contestant => {
           return (
-            <Link to={'/contestant/' + contestant.id}>
-              <ContestantSummary contestant={contestant} key = {contestant.id}/>
+            <Link to={"/contestant/" + contestant.id} key={contestant.id}>
+              <ContestantSummary contestant={contestant} />
             </Link>
-          )
-        })} 
-      </div>
-  )
-}
+          );
+        })}
+    </div>
+  );
+};
 
 export default ContestantList;
--- src/Components/dashboard/Dashboard.js
+++ src/Components/dashboard/Dashboard.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Notifications from "./Notification";
-import ContestantList from '../contestants/contestantsList';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
-
+import ContestantList from "../contestants/contestantsList";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 class Dashboard extends Component {
-
-
   render() {
     const { contestants } = this.props;
 
@@ -16,7 +13,7 @@ class Dashboard extends Component {
       <div className=" dashboard container">
         <div className="row">
           <div className="col s12 m6">
-          <h5 className="red-text text-darken-3">Contestant List</h5>
+            <h5 className="red-text text-darken-3">Contestant List</h5>
             <ContestantList contestants={contestants} />
           </div>
           <div className="col s12 m5 offset-m1">
@@ -24,20 +21,17 @@ class Dashboard extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  console.log(state);
+const mapStateToProps = state => {
   return {
     contestants: state.firestore.ordered.contestants
-  }
-}
+  };
+};
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([
-    { collection: 'contestants' }
-  ])
+  firestoreConnect([{ collection: "contestants" }])
 )(Dashboard);
