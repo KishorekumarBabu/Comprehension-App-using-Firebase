119d13bf776178d0028b927810a048cf7d3244be
KishorekumarBabu <50402067+KishorekumarBabu@users.noreply.github.com>
2021-09-26 18:33:59 +0530

    Updated Passage Details

--- src/Components/passage/passageDetails/index.js
+++ src/Components/passage/passageDetails/index.js
@@ -3,18 +3,12 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import Quiz from "../../quiz";
-import M from "materialize-css";
 import { currentPage } from "../../../store/actions/pageActions";
 import { Redirect } from "react-router-dom";
 import Preloader from "../../layout/preloader";
 import "./index.css";
 
 class PassageDetails extends Component {
-  componentDidMount() {
-    this.props.currentPage("PASSAGE_DETAILS");
-    M.Tabs.init(this.Tabs);
-  }
-
   render() {
     const { passage, timerDetails, auth } = this.props;
     if (!auth.uid) return <Redirect to="/signin" />;
---
+++ src/sample.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDnOmfWXucHEw6CfNoycpwMtdGh4PLqxzE",
+  authDomain: "super-mobile-store.firebaseapp.com",
+  databaseURL: "https://super-mobile-store.firebaseio.com",
+  projectId: "super-mobile-store",
+  storageBucket: "super-mobile-store.appspot.com",
+  messagingSenderId: "822904625312",
+  appId: "1:822904625312:web:3c568bf7a0d850a30651c5",
+  measurementId: "G-VT301DEW0V"
+};
